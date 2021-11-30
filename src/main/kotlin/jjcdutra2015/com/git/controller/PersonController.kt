fd7a3e2868dc1bb80425c2772acdc30b9b1b2ffc
Julio Dutra <jjcdutra@gmail.com>
2021-11-30 20:43:41 -0300

    Mais ajustes no controller

--- src/main/kotlin/jjcdutra2015/com/git/controller/PersonController.kt
+++ src/main/kotlin/jjcdutra2015/com/git/controller/PersonController.kt
@@ -2,7 +2,6 @@ package jjcdutra2015.com.git.controller
 
 import jjcdutra2015.com.git.model.Person
 import jjcdutra2015.com.git.service.PersonService
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,35 +10,27 @@ class PersonController(
     private val service: PersonService
 ) {
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): Person {
         return service.findById(id)
     }
 
-    @RequestMapping(method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping
     fun findAll(): List<Person> {
         return service.findAll()
     }
 
-    @RequestMapping(
-        method = [RequestMethod.POST],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
+    @PostMapping
     fun create(@RequestBody person: Person): Person {
         return service.create(person)
     }
 
-    @RequestMapping(
-        method = [RequestMethod.PUT],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
+    @PutMapping
     fun update(@RequestBody person: Person): Person {
         return service.update(person)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE])
+    @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long) {
         service.delete(id)
     }
