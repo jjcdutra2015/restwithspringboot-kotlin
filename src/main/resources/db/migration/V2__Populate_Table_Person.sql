e06b76f61c97d767e01432540ad7b3ed49658914
Julio Dutra <jjcdutra@gmail.com>
2021-12-11 10:33:26 -0300

    Configuração flyway migration

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Carmo - Rio de Janeiro - Brasil', 'Leandro', 'Male', 'Costa'),
+	(2, 'Carmo - Rio de Janeiro - Brasil', 'Gabriela', 'Female', 'Costa'),
+	(3, 'Carmo - Rio de Janeiro - Brasil', 'Flavio', 'Male', 'Costa'),
+	(4, 'Carmo - Rio de Janeiro - Brasil', 'Fernanda', 'Female', 'Cardoso da Silva'),
+	(5, 'Carmo - Rio de Janeiro - Brasil', 'Pedro', 'Male', 'Paulo'),
+	(6, 'Carmo - Rio de Janeiro - Brasil', 'Marcos', 'Male', 'Paulo');
