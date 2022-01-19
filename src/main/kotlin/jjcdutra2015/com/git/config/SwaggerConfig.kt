d091a4b69f6b62518ccbe91a65c8671e4f5d3613
Julio Dutra <jjcdutra@gmail.com>
2022-01-19 20:47:26 -0300

    Ajustando configuração do swagger

---
+++ src/main/kotlin/jjcdutra2015/com/git/config/SwaggerConfig.kt
@@ -0,0 +1,40 @@
+package jjcdutra2015.com.git.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("jjcdutra2015.com.git"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo())
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfo(
+            "RESTfulL API with Spring Boot ",
+            "Some description about your API.",
+            "v1",
+            "Termos of service URL",
+            Contact("Julo", "git.com/jjcdutra2015", "email@email.com"),
+            "License of API",
+            "License of terms",
+            emptyList()
+        )
+    }
+
+}
--- src/main/kotlin/jjcdutra2015/com/git/controller/PersonController.kt
+++ src/main/kotlin/jjcdutra2015/com/git/controller/PersonController.kt
@@ -1,9 +1,12 @@
 package jjcdutra2015.com.git.controller
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import jjcdutra2015.com.git.data.vo.PersonVO
 import jjcdutra2015.com.git.service.PersonService
 import org.springframework.web.bind.annotation.*
 
+@Api(value = "Person API", description = "Persons descriptions", tags = ["Person API"])
 @RestController
 @RequestMapping("/api/person/v1")
 class PersonController(
@@ -15,6 +18,7 @@ class PersonController(
         return service.findById(id)
     }
 
+    @ApiOperation("Find all record people")
     @GetMapping(produces = ["application/json", "application/xml"])
     fun findAll(): List<PersonVO> {
         return service.findAll()
