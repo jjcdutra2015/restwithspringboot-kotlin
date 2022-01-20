7af81bc4a3dac350db7062b0674aa5c4a5ce71d9
Julio Dutra <jjcdutra@gmail.com>
2022-01-20 19:40:39 -0300

    Habilitando CORS globalmente

--- src/main/kotlin/jjcdutra2015/com/git/config/WebConfig.kt
+++ src/main/kotlin/jjcdutra2015/com/git/config/WebConfig.kt
@@ -3,12 +3,18 @@ package jjcdutra2015.com.git.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+    }
 
     @Override
     fun configurerContentNegotiation(configurer: ContentNegotiationConfigurer) {
