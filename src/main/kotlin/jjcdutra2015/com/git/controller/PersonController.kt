e0b9a1b579ea4b313f6eb127b3f82107b72fba24
Julio Dutra <jjcdutra@gmail.com>
2022-01-20 19:32:14 -0300

    Habilitando CORS no controller e nos métodos do controller

--- src/main/kotlin/jjcdutra2015/com/git/controller/PersonController.kt
+++ src/main/kotlin/jjcdutra2015/com/git/controller/PersonController.kt
@@ -7,17 +7,20 @@ import jjcdutra2015.com.git.service.PersonService
 import org.springframework.web.bind.annotation.*
 
 @Api(value = "Person API", description = "Persons descriptions", tags = ["Person API"])
+//@CrossOrigin
 @RestController
 @RequestMapping("/api/person/v1")
 class PersonController(
     private val service: PersonService
 ) {
 
+    //@CrossOrigin(origins = ["http://localhost:8080", "https://google.com.br"])
     @GetMapping(value = ["/{id}"], produces = ["application/json", "application/xml"])
     fun findById(@PathVariable id: Long): PersonVO {
         return service.findById(id)
     }
 
+    //@CrossOrigin(origins = ["http://localhost:8080"])
     @ApiOperation("Find all record people")
     @GetMapping(produces = ["application/json", "application/xml"])
     fun findAll(): List<PersonVO> {
