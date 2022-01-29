3e556dbd698f53441f9bcb467a466e29beaf33ed
Julio Dutra <jjcdutra@gmail.com>
2022-01-29 10:50:23 -0300

    Implementando segurança

---
+++ src/main/kotlin/jjcdutra2015/com/git/config/SecurityConfig.kt
@@ -0,0 +1,37 @@
+package jjcdutra2015.com.git.config
+
+import jjcdutra2015.com.git.security.jwt.JwtConfigurer
+import jjcdutra2015.com.git.security.jwt.JwtTokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+class SecurityConfig(
+    private val tokenProvider: JwtTokenProvider
+) : WebSecurityConfigurerAdapter() {
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager = super.authenticationManagerBean()
+
+    protected fun configurer(http: HttpSecurity) {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/auth/signin", "/api-docs/**", "/swagger-ui.html**").permitAll()
+            .antMatchers("/api/**").authenticated()
+            .antMatchers("/users").denyAll()
+            .and()
+            .apply(JwtConfigurer(tokenProvider))
+    }
+}
